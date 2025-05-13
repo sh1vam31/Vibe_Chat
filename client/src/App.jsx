b517fb9e41b84a41d2cba64be4e2c78e0f058c7a
deep <www.crushy@gmail.com>
2025-05-13 09:37:52 +0530

    Created component for ChatPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
+import ChatPage from './pages/ChatPage'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-[url('https://i.pinimg.com/736x/d0/5c/a8/d05ca86bbbca5e3dcc12205a1f67b224.jpg')] bg-contain">
+       {/* setting routes for every pages */}
+
+      <Routes>
+       <Route path='/' element={<ChatPage/>} />
+       <Route path='/login' element={<LoginPage/>} />
+       <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
+
+    </div>
   )
 }
 
