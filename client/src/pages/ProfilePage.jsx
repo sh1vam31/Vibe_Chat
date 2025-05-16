6e16834ec17a1819e689951027095136121e42bb
deep <www.crushy@gmail.com>
2025-05-16 10:53:41 +0530

    Changing profile pic feature added

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -54,7 +54,7 @@ const ProfilePage = () => {
 
            <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer' >Save</button>
         </form>
-        <img className={` max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 transition-transform duration-300 ease-in-out hover:scale-110 hover:rotate-1 hover:shadow-2xl  ${selectedImg && 'rounded-full'} `} src={assets.logo_icon} alt="" />
+        <img className={` max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 transition-transform duration-300 ease-in-out hover:scale-110 hover:rotate-1 hover:shadow-2xl  ${selectedImg && 'rounded-full'} `} src={authUser?.profilePic || assets.logo_icon} alt="" />
      </div>
     </div>
   )
